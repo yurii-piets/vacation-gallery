9472c874ee573c08b29697e70b05c1d3be81c4f4
Yurii Piets <yurii.piets@gmail.com>
2020-01-20 11:41:21 +0100

    Updates

---
+++ precache-manifest.42d3884f58209ac8cd43ce7bb7f05ca2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f923cc0c560658735ec1f82564e35de",
+    "url": "/vacation-gallery/index.html"
+  },
+  {
+    "revision": "dd8174caac76f27cc8a5",
+    "url": "/vacation-gallery/static/css/2.8495ce49.chunk.css"
+  },
+  {
+    "revision": "e62db786c03a7d17cd30",
+    "url": "/vacation-gallery/static/css/main.2af6ffc0.chunk.css"
+  },
+  {
+    "revision": "dd8174caac76f27cc8a5",
+    "url": "/vacation-gallery/static/js/2.7f5038ab.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/vacation-gallery/static/js/2.7f5038ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e62db786c03a7d17cd30",
+    "url": "/vacation-gallery/static/js/main.76634195.chunk.js"
+  },
+  {
+    "revision": "b9fd72060a7ebbb98aae",
+    "url": "/vacation-gallery/static/js/runtime-main.20d4a8f6.js"
+  },
+  {
+    "revision": "c65100e9d6c73976e9d67e96def9457c",
+    "url": "/vacation-gallery/static/media/loading-placeholder.c65100e9.gif"
+  }
+]);
